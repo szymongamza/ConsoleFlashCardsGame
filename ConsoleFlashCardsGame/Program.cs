7bdbf362d93180c035093d8fe9599f487650bea5
Szymon Gamża <gamza.szymon@gmail.com>
2022-08-23 15:56:09 +0200

    Dodaj pliki projektów.

---
+++ ConsoleFlashCardsGame/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using QC = Microsoft.Data.SqlClient;
+
+namespace ConsoleFlashCardsGame
+{
+	public class Program
+	{
+		static public void Main()
+		{
+			using (var connection = new QC.SqlConnection(
+				"Server=(LocalDb)\\FlashCardsDB;" +
+				"Database=FlashCardsDb;"
+				))
+			{
+				connection.Open();
+				Console.WriteLine("Connected successfully.");
+
+				Console.WriteLine("Press any key to finish...");
+				Console.ReadKey(true);
+			}
+		}
+	}
+}
+/**** Actual output:  
+Connected successfully.  
+Press any key to finish...  
+****/  
